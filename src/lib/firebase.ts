d4aaa256b8667dd6878e368c8ec008a5bc8293b2
Sakthi <jerrycad01@gmail.com>
2025-12-14 06:55:24 +0000

    use this import { initializeApp } from "firebase/app"; // TODO: Add SDK

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,17 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBJRtNPUHp3ekcsHOxWd7M2SmD3H2FDdEw",
   authDomain: "rupiece-ecactly-what-you-want.firebaseapp.com",
   projectId: "rupiece-ecactly-what-you-want",
-  storageBucket: "rupiece-ecactly-what-you-want.firebasestorage.app",
+  storageBucket: "rupiece-ecactly-what-you-want.appspot.com",
   messagingSenderId: "788489947271",
   appId: "1:788489947271:web:75c3adf0f86fa5555f9022"
 };
 
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const firestore = getFirestore(app);
